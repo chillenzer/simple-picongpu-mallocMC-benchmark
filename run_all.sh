62702f5da99422de9bae6b9b131dfd50dcbd8112
Julian Lenz <j.lenz@hzdr.de>
2024-12-03 15:47:51 +0100

    run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -x
+
+PROFILE=$1
+FLAGSFOLDER=$(pwd -P)/$2
+
+EXAMPLES=("KelvinHelmholtz" "FoilLCT")
+ALGORITHMS=("FlatterScatter" "ScatterAlloc" "Gallatin")
+
+mkdir -p output
+for example in ${EXAMPLES[@]}; do
+  mkdir -p output/$example
+  for algorithm in ${ALGORITHMS[@]}; do
+    echo "=============================="
+    echo "Running example: $example"
+    echo "Using algorithm: $algorithm"
+    echo "=============================="
+    DATETIME=$(date +"%Y-%m-%d %H:%M:%S")
+    bash run_folder.sh build/$example/$algorithm $FLAGSFOLDER/${example}.flags $PROFILE
+    echo ""
+  done
+done
