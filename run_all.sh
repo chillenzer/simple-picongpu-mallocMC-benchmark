9be6f491e8d1522193e9323c9b257508efa5b9f1
Julian Lenz <j.lenz@hzdr.de>
2024-12-04 15:17:53 +0100

    Remove redundant folder creations

--- run_all.sh
+++ run_all.sh
@@ -6,9 +6,7 @@ FLAGSFOLDER=$(pwd -P)/$2
 EXAMPLES=("KelvinHelmholtz" "FoilLCT")
 ALGORITHMS=("FlatterScatter" "ScatterAlloc" "Gallatin")
 
-mkdir -p output
 for example in ${EXAMPLES[@]}; do
-  mkdir -p output/$example
   for algorithm in ${ALGORITHMS[@]}; do
     echo "=============================="
     echo "Running example: $example"
