00f6c20b0944f44a96b4de5fa8b2daf2804f4cbf
Julian Lenz <j.lenz@hzdr.de>
2026-08-21 10:37:17 +0200

    Add log_run_rosi.sh

---
+++ log_run_rosi.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+FOLDER="output/rosi-sleeptimes"
+FILENAME="$FOLDER/run_$(date --rfc-3339=seconds | sed 's/ /_/g').txt"
+mkdir -p "$FOLDER"
+
+echo "========================" | tee -a $FILENAME
+echo "Logging environment" | tee -a $FILENAME
+echo "========================" | tee -a $FILENAME
+
+git log -n1 | tee -a $FILENAME
+git diff | tee -a $FILENAME
+nvidia-smi | tee -a $FILENAME
+
+echo "========================" | tee -a $FILENAME
+echo "Starting run" | tee -a $FILENAME
+echo "========================" | tee -a $FILENAME
+
+bash run_all.sh profiles/rosi-v100.profile flags/ 2>&1 | tee -a $FILENAME
